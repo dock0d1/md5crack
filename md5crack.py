0056c941d1fa6699bd0f4d76c026456c37ea7e01
dock0d1 <43358190+dock0d1@users.noreply.github.com>
2021-03-26 11:14:39 -0300

    Add files via upload

---
+++ md5crack.py
@@ -0,0 +1,24 @@
+import hashlib
+
+print('''
+
+██████╗  ██████╗  ██████╗██╗  ██╗ ██████╗ ██████╗  ██╗
+██╔══██╗██╔═══██╗██╔════╝██║ ██╔╝██╔═████╗██╔══██╗███║
+██║  ██║██║   ██║██║     █████╔╝ ██║██╔██║██║  ██║╚██║
+██║  ██║██║   ██║██║     ██╔═██╗ ████╔╝██║██║  ██║ ██║
+██████╔╝╚██████╔╝╚██████╗██║  ██╗╚██████╔╝██████╔╝ ██║
+╚═════╝  ╚═════╝  ╚═════╝╚═╝  ╚═╝ ╚═════╝ ╚═════╝  ╚═╝
+  
+	''')
+
+lista=input("wordlist: ")
+hash2crack=input("hash: ")
+
+linhasDeLista=open(lista, "r").readlines()
+
+for i in range(0, len(linhasDeLista)):
+	hash2comp=hashlib.md5(linhasDeLista[i].replace("\n","").encode()).hexdigest()
+	if hash2crack == hash2comp:
+		print("Senha Encontrada: " +linhasDeLista[i].replace("\n",""))
+		exit()
+print("Senha Não Encontrada")
